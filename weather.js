a273700590ca582ce2150adb8bca2d4ee684651a
obkin <kapellwork@gmail.com>
2023-02-23 19:34:49 +0200

    added: helpers, args.js / changed: package.json, weather.js

--- weather.js
+++ weather.js
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
+import { getArgs } from "./helpers/args.js";
+
+const initCli = () => {
+    const agrs = getArgs(process.argv);
+    console.log(agrs);
+    if (args.h) {
+        // Виводимо help
+    }
+    if (args.s) {
+        // Зберігаємо місто
+    }
+    if (args.t) {
+        // Зберігаємо токен
+    }
+    // Вивести погоду (якщо аргументи не були передані)
+};
+
+initCli();
 
