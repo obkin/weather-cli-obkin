da47737eb38cc62981c557b8629c392810a0dd1d
obkin <kapellwork@gmail.com>
2023-02-28 16:54:16 +0200

    added: service/storage.service.js

---
+++ services/storage.service.js
@@ -0,0 +1,11 @@
+import { homedir } from 'os';
+import { join } from 'path';
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const saveKeyValue = (key, value) => {
+
+};
+
+export { saveKeyValue };
+
--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
 import { printError, printSuccess, printHelp } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
 
 const initCli = () => {
     const args = getArgs(process.argv);
@@ -11,7 +12,7 @@ const initCli = () => {
         // Зберігаємо місто
     }
     if (args.t) {
-        // Зберігаємо токен
+        saveKeyValue('token', args.t);
     }
     // Вивести погоду (якщо аргументи не були передані)
 };
