1d84f1ff539531e52f2703b445c5c4cfca430b42
obkin <kapellwork@gmail.com>
2023-03-03 15:59:05 +0200

    changed: storage.service.js, weather.js / token saving

--- helpers/args.js
+++ helpers/args.js
@@ -16,4 +16,3 @@ const getArgs = (args) => {
 };
 
 export { getArgs };
-
--- weather.js
+++ weather.js
@@ -3,6 +3,15 @@ import { getArgs } from "./helpers/args.js";
 import { printError, printSuccess, printHelp } from "./services/log.service.js";
 import { saveKeyValue } from "./services/storage.service.js";
 
+const saveToken = async (userToken) => {
+    try {
+        await saveKeyValue('token', userToken);
+        printSuccess('The token was saved ;)');
+    } catch(e) {
+        printError(e.message);
+    }
+};
+
 const initCli = () => {
     const args = getArgs(process.argv);
     if (args.h) {
@@ -12,10 +21,9 @@ const initCli = () => {
         // Зберігаємо місто
     }
     if (args.t) {
-        saveKeyValue('token', args.t);
+        return saveToken(args.t);
     }
     // Вивести погоду (якщо аргументи не були передані)
 };
 
 initCli();
-
